32ebdb963d2b81fc3d6fb31cbe55b05e2407936f
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-31 09:58:57 -0300

    feat: implementa a configuração inicial do teste (sujeita a mais alterações)

---
+++ config.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+# URLs para download
+DEMONSTRACOES_URL = "https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/"
+OPERADORAS_URL = "https://dadosabertos.ans.gov.br/FTP/PDA/operadoras_de_plano_de_saude_ativas/"
+
+# Configurações gerais
+PASTA_DOWNLOAD = "downloads"  # Diretório base para armazenar os arquivos baixados
+MAX_WORKERS = 4  # Número de downloads simultâneos
+
+# Anos para filtrar os arquivos de demonstrações contábeis
+ANO_ATUAL = datetime.now().year
+ANOS = [ANO_ATUAL - 1, ANO_ATUAL - 2]
